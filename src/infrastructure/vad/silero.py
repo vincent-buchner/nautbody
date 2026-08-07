a2031434cfacf60eeee883867822285182c49036
vincent <vincentbuchner01@gmail.com>
2026-08-06 22:08:47 -0500

    added silero vad infrastructure

---
+++ src/infrastructure/vad/silero.py
@@ -0,0 +1,16 @@
+import numpy as np
+from silero_vad import VADIterator, load_silero_vad
+
+
+class SileroVAD:
+    def __init__(self, sample_rate: int) -> None:
+        self._model = load_silero_vad()
+        # TODO: Add better vad settings for padding speech
+        self._vad = VADIterator(self._model, sampling_rate=sample_rate)
+
+    def process_audio_chunk(self, audio_bytes: np.ndarray):
+        event = self._vad(audio_bytes, return_seconds=True)
+        return event
+
+    def reset(self):
+        self._vad.reset_states()
