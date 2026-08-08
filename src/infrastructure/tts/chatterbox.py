19bbd7bba306a6e7ffb39432dbec4b654274566a
vincent <vincentbuchner01@gmail.com>
2026-08-08 14:36:13 -0500

    added chatterbox infra, supported streaming

---
+++ src/infrastructure/tts/chatterbox.py
@@ -0,0 +1,27 @@
+from collections.abc import Generator
+from pathlib import Path
+
+from chatterbox.tts import ChatterboxTTS
+
+
+class ChatterboxTTSInfra:
+    def __init__(
+        self, sample_audio_path: Path | str, verbose_generation: bool = False
+    ) -> None:
+        self._model = ChatterboxTTS.from_pretrained(device="cuda")
+        self._model.prepare_conditionals(sample_audio_path)
+        self._verbose_generation = verbose_generation
+
+    def generate_audio(self, text: str) -> Generator[bytes, None, list[bytes]]:
+        full_audio_collection: list[bytes] = []
+
+        for audio_tensor_chunk, metrics in self._model.generate_stream(text):
+            audio_bytes_chunk = audio_tensor_chunk.numpy().tobytes()
+            full_audio_collection.append(audio_bytes_chunk)
+
+            if self._verbose_generation:
+                print(f"Audio chunk generated:\n{metrics}")
+
+            yield audio_bytes_chunk
+
+        return full_audio_collection
